1ecebb6e98d4f48f341b0b33b1fb47f7db468548
JameAHoang <64461251+JameAHoang@users.noreply.github.com>
2021-07-21 22:10:08 +0700

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,7 @@ const routes: Routes = [
     path: 'students',
     loadChildren: () =>
       import('./students/students.module').then((m) => m.StudentsModule),
+    canActivate:[AuthGuard]
   },
 
   {
